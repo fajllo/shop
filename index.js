7a54991a0335116fccda196d0bcf0792509356fd
fsdr <filipsordyl8@unilever.com>
2020-10-03 11:53:26 +0200

    basic of express

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hi all!');
+  res.send(`   
+ <div>
+  <form method="POST">
+    <input type="text" name="email" placeholder="email" id="" />
+    <input type="text" name="password" placeholder="password" id="" />
+    <input type="text" name="confirm" placeholder="confirm" id="" />
+    <button>Sing UP!</button>
+  </form>
+</div>`);
 });
 
+app.post('/', (req, res) => {
+  res.send('Account created!');
+});
 app.listen(3000, () => {
   console.log('listening dude');
 });
