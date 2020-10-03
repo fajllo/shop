5d000904dbee67510d17740db61d625ee7e696f7
fsdr <filipsordyl8@unilever.com>
2020-10-03 11:27:07 +0200

    express setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi there!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi all!');
+});
+
+app.listen(3000, () => {
+  console.log('listening dude');
+});
