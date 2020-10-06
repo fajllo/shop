21c9260f5066d5689f61345f05aff5cd936e7864
fsdr <filipsordyl8@unilever.com>
2020-10-06 22:06:16 +0200

    new packages cookie sessien nie działa

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
+const session = require('express-session');
 const usersRepo = require('./repositories/users');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 //encrypting cookies
-app.use(cookieSession({keys: ['asdsafwquyg123e1equwh9']}));
+app.use(session({secret: 'secret,shhhh!', resave: false}));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
+app.get('/singup', (req, res) => {
   res.send(`   
  <div>
   <form method="POST">
@@ -20,7 +22,7 @@ app.get('/', (req, res) => {
 </div>`);
 });
 
-app.post('/', async (req, res) => {
+app.post('/singup', async (req, res) => {
   // get accset to data send
   // console.log(req.body);
   const {email, password, confirm} = req.body;
@@ -34,10 +36,26 @@ app.post('/', async (req, res) => {
   //create user in our repo
   const user = await usersRepo.create({email, password});
   // added by cookie session
-  // req.session === {}
-  req.session.userId === user.id;
+  // req.session === {
 
   res.send('Account created!');
+});
+app.get('/signin', (req, res) => {
+  res.send(`   
+  <div>
+   <form method="POST">
+     <input type="text" name="email" placeholder="email" id="" />
+     <input type="text" name="password" placeholder="password" id="" />
+     <button>Sing in!</button>
+   </form>
+ </div>`);
+});
+
+app.post('/signin', async (req, res) => {});
+
+app.get('/singout', (req, res) => {
+  req.session = null;
+  res.send('logged out!');
 });
 
 app.listen(3000, () => {
