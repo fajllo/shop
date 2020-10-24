02ee80e2ac195ce38731df4f59c75a6a87e5d4f6
fsdr <filipsordyl8@unilever.com>
2020-10-24 22:37:39 +0200

    🥼🧥 product validation

--- routs/admin/products.js
+++ routs/admin/products.js
@@ -3,12 +3,20 @@ const {route} = require('./auth');
 const router = express.Router();
 const stockRepo = require('../../repositories/stock');
 const newProductTemp = require('../../viwes/admin/products/newstuff');
+const {requireTitle, requirePrice} = require('./validators');
+const {check, validationResult} = require('express-validator');
 
 router.get('/admin/products', (req, res) => {
   res.send('porduct');
 });
 router.get('/admin/products/new', (req, res) => {
   res.send(newProductTemp({}));
+});
+
+router.post('/admin/products/new', [requirePrice, requireTitle], (req, res) => {
+  const errors = validationResult(req);
+  console.log(errors);
+  res.send('dodano produkts');
 });
 
 module.exports = router;
--- viwes/admin/products/newstuff.js
+++ viwes/admin/products/newstuff.js
@@ -3,11 +3,11 @@ const {getError} = require('../../helpers');
 module.exports = ({errors}) => {
   return layout({
     content: `   
-      <form action="post">
+      <form method="POST" >
         <input type="text" placeholder="Title" name="title" />
         <input type="text" placeholder="Price" name="price" />
         <input type="file" name="image" />
-        <button>submit!</button>
+        <button>Add new!</button>
       </form>`,
   });
 };
